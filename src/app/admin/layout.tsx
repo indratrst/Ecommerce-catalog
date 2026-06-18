3b906d702305c009253b3436d1b28d5bfabd9fd6
Indra Tristia <tristia71@gmail.com>
2026-06-18 18:06:04 +0700

    update : apply package sooner in admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import { AdminSidebar } from "@/components/admin/AdminSidebar";
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { Toaster } from "sonner";
 
 export default async function AdminLayout({
   children,
@@ -17,9 +18,8 @@ export default async function AdminLayout({
     <div className="flex min-h-screen bg-slate-50 dark:bg-[#0f172a]">
       <AdminSidebar role={session.role} userName="Administrator" />
       <main className="flex-1 overflow-y-auto h-screen p-8">
-        <div className="max-w-7xl mx-auto">
-          {children}
-        </div>
+        <div className="max-w-7xl mx-auto">{children}</div>
+        <Toaster richColors position="top-right" />
       </main>
     </div>
   );
